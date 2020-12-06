8220c919615f572111cad68ad909ff3ddb739f1c
Jtsw <jtsw1990@gmail.com>
2020-12-06 22:47:31 +1100

    adjusted unit test for max_capacity input

--- test/test_storage.py
+++ test/test_storage.py
@@ -1,31 +1,21 @@
 import unittest
 import sys
 import os
+import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from vending_machine import vending_machine.VendingMachine
+from vending_machine.vending_machine import VendingMachine, DataReader
 
 
 class TestingStorageBounds(unittest.TestCase):
-
-    def test_max_storage(self):
-			vm_instance = VendingMachine("drinks_list.csv")
-			total_stock = vm_instance._current_stock
-			max_storage = total_stock - 1
-			self.assertTrue(
-				total_stock <= max_storage, "Stock list exceeds vending machine max storage"
-			)
-		'''
-        with self.assertRaises(ValueError) as exception_context:
-            add_fish_to_aquarium(fish_list=too_many_fish)
-        self.assertEqual(
-            str(exception_context.exception),
-            "A maximum of 10 fish can be added to the aquarium"
-			'''
-
-			
-    def test_min_storage(self):
-        pass
+	
+	def test_capacity_input(self):
+		self.assertRaises(TypeError, 1 + "str")
+				
+		#test_instance = VendingMachine(150)
+		#max_capacity = test_instance.max_capacity
+		#self.assertEqual(isinstance(max_capacity, (int, float)), True)
 
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
+	
