dc17fbe10e9c5c457dad088e3c59a922942d07ca
Jtsw <jtsw1990@gmail.com>
2020-12-07 00:05:01 +1100

    Adjusted capacity unit test
    Added class methods for setup and teardown

--- test/test_storage.py
+++ test/test_storage.py
@@ -2,12 +2,26 @@ import unittest
 import sys
 import os
 import numpy as np
+import csv
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from vending_machine.vending_machine import VendingMachine, DataReader
 
 
 class TestingCapacty(unittest.TestCase):
 	
+	@classmethod
+	def setUpClass(cls):
+		with open("testing_df.csv", mode="w+") as csv_file:
+			colnames = ["name", "price_per_unit", "no_of_units"]
+			writer = csv.DictWriter(csv_file, fieldnames=colnames)
+			writer.writeheader()
+			writer.writerow({"name": "coke", "price_per_unit": 1, "no_of_units": 500})
+		
+	@classmethod
+	def tearDownClass(cls):
+		os.remove("testing_df.csv")
+
+
 	def test_capacity_input(self):
 		for scenario in np.linspace(-2, 2, 9):
 			if scenario < 0:
@@ -16,12 +30,12 @@ class TestingCapacty(unittest.TestCase):
 				self.assertRaises(TypeError, VendingMachine, scenario)
 			else:
 				self.assertEqual(scenario, VendingMachine(scenario).max_capacity)
-				
+
 	def test_capacity_bound(self):
-		# TODO: create csv to for negative test case
-		pass
-		
+		scenario = VendingMachine(200)
+		self.assertRaises(ValueError, scenario.load_drinks, "testing_df.csv")
 
 
 if __name__ == "__main__":
 	unittest.main()
+
